e975a52638515aaf7fd0857f99d4e26cc2ddd6f2
nyila7 <nyilas.kristof.agoston@gmail.com>
2024-02-15 21:44:47 +0100

    GUI basic layout szervezo
    Co-authored-by: notyasuomain-cmd <notyasuomain-cmd@users.noreply.github.com>

--- mainGUI.py
+++ mainGUI.py
@@ -5,28 +5,43 @@ import fogadoGUI
 
 class App(customtkinter.CTk):
     def __init__(self):
-        super().__init__()
+        customtkinter.CTk.__init__(self)
 
-        self.title("Fogadás kezelő 6000 (x64) NOT REGISTERED")
+        self.title("Fogadói rendszer")
         self.geometry("1280x720")
 
+        container = customtkinter.CTkFrame(self)  
+        container.pack(side = "top", fill = "both", expand = True) 
+
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)        
+
+        self.frames = {}
+
+        for F in (menuFrame, szervezoGUI.SzervezoFrame, fogadoGUI.FogadoFrame):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+
+        self.show_frame(menuFrame)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+        
+class menuFrame(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        customtkinter.CTkFrame.__init__(self, parent)
+        
         self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        szerepkorFont = ("Product Sans", 30)
+        szerepkorFont = ("Comic Sans MS", 30)
 
-        self.buttonL = customtkinter.CTkButton(self, text="Szervező", corner_radius=10, font=szerepkorFont, command=self.szervezo)
+        self.buttonL = customtkinter.CTkButton(self, text="Szervező", corner_radius=10, font=szerepkorFont, command=lambda : controller.show_frame(szervezoGUI.SzervezoFrame))
         self.buttonL.grid(row=0, column=0, padx=10, pady=10, sticky="news")
-        self.buttonR = customtkinter.CTkButton(self, text="Fogadó", corner_radius=10, font=szerepkorFont, command=self.fogado)
-        self.buttonR.grid(row=0, column=1, padx=10, pady=10, sticky="nesw")
-
-    def szervezo(self):
-        app.destroy()
-        szervezoGUI.indul()
-    
-    def fogado(self):
-        app.destroy()
-        fogadoGUI.indul()
-
+        self.buttonR = customtkinter.CTkButton(self, text="Fogadó", corner_radius=10, font=szerepkorFont, command=lambda : controller.show_frame(fogadoGUI.FogadoFrame))
+        self.buttonR.grid(row=0, column=1, padx=10, pady=10, sticky="nesw")        
 app = App()
 app.mainloop()
--- szervezoGUI.py
+++ szervezoGUI.py
@@ -1,20 +1,39 @@
 import customtkinter
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("asd")
-        self.geometry("1280x720")
-
-        self.grid_columnconfigure((0, 1), weight=1)
+class SzervezoFrame(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        customtkinter.CTkFrame.__init__(self, parent)
+        
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=16)
+        self.grid_columnconfigure(2, weight=16)
         self.grid_rowconfigure(0, weight=1)
 
-        szerepkorFont = ("Product Sans", 30)
 
-        self.buttonL = customtkinter.CTkButton(self, text="Szervező", corner_radius=10, font=szerepkorFont)
-        self.buttonL.grid(row=0, column=0, padx=10, pady=10, sticky="news")
 
-def indul():
-    app = App()
-    app.mainloop()
+        szerepkorFont = ("Comic Sans MS", 30)
+
+        #oldal bar?
+        bar = customtkinter.CTkFrame(self)
+        bar.grid(row=0, column=0, padx=10, pady=10, sticky="nesw")
+        bar.columnconfigure(0, weight=1)
+
+        letrehozas_button = customtkinter.CTkButton(bar, text="Új fogadás", corner_radius=10, font=szerepkorFont, fg_color="transparent", hover_color="gray")
+        letrehozas_button.grid(row=0, column=0, padx=10, pady=10)
+
+        # form
+        form = customtkinter.CTkFrame(self)
+        form.grid(row=0, column=1, padx=10, pady=10, sticky="nesw")
+
+        form.grid_columnconfigure(0, weight=1)
+        form.grid_columnconfigure(1, weight=1)
+        
+        
+        
+        jatek_megvenezes_label = customtkinter.CTkLabel(form, text="Játék megnevezése", font=szerepkorFont)
+        jatek_megvenezes_label.grid(row=0, column=0, padx=10, pady=10, columnspan=2, sticky="nesw")
+
+        self.jatek_megnevezes_input = customtkinter.CTkEntry(form, font=szerepkorFont, width=400, placeholder_text="Lajos és Bettina programjának futása")
+        self.jatek_megnevezes_input.grid(row=1, column=0, padx=10, pady=10, columnspan=2)
+
+        # jelenlegi gambok
