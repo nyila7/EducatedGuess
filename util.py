9e74e1c2df6a82108afe483d060b83fc28fde1a9
notyasuomain-cmd <notyasuomain7@gmail.com>
2024-02-21 20:35:03 +0100

    unused imports

--- util.py
+++ util.py
@@ -1,18 +1,15 @@
 import customtkinter
-import random
-from fajlkezeles import ir
-from jatek_lezarasa import esemenyek_lekerdez, pontszamitas, szemelyek_lekerdez, szorzo_szamitas
 
 
 def populate_games(self) -> None:
     with open("jatekok.txt", mode="r", encoding="utf-8") as f:
-            for i, sor in enumerate(f):
-                if sor.find(";") != -1:
-                    self.jatekok_szamolo += 1
-                    jelenlegi_jatekok_list = customtkinter.CTkLabel(self.jelenlegi_jatekok, text=sor.split(";")[1], font=self.fonts, fg_color="gray", corner_radius=10)
-                    jelenlegi_jatekok_list.grid(row=self.jatekok_szamolo, column=0, padx=10, pady=10, sticky="nesw")
-                    jelenlegi_jatek_lezaras_butt = customtkinter.CTkButton(self.jelenlegi_jatekok, text="lezárás", font=self.fonts, command=lambda x = sor.split(";")[1], y = self.jatekok_szamolo: self.jatek_lezaras(x,y), fg_color="red", hover_color="gray")
-                    jelenlegi_jatek_lezaras_butt.grid(row=self.jatekok_szamolo, column=1, padx=10, pady=10, sticky="nesw")
+        for _, sor in enumerate(f):
+            if sor.find(";") != -1:
+                self.jatekok_szamolo += 1
+                jelenlegi_jatekok_list = customtkinter.CTkLabel(self.jelenlegi_jatekok, text=sor.split(";")[1], font=self.fonts, fg_color="gray", corner_radius=10)
+                jelenlegi_jatekok_list.grid(row=self.jatekok_szamolo, column=0, padx=10, pady=10, sticky="nesw")
+                jelenlegi_jatek_lezaras_butt = customtkinter.CTkButton(self.jelenlegi_jatekok, text="lezárás", font=self.fonts, command=lambda x = sor.split(";")[1], y = self.jatekok_szamolo: self.jatek_lezaras(x,y), fg_color="red", hover_color="gray")
+                jelenlegi_jatek_lezaras_butt.grid(row=self.jatekok_szamolo, column=1, padx=10, pady=10, sticky="nesw")
 
 def populate_games_fogado(self) -> None:
     with open("jatekok.txt", mode="r", encoding="utf-8") as f:
